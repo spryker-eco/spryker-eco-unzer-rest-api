b82dc5f474a61b8a54643c917ac89582552cd1ef
Kostiantyn Kichehlov <kostiantyn.kichehlov@spryker.com>
2022-05-12 09:10:37 +0200

    CC-14610 Added tester

---
+++ tests/SprykerEcoTest/Glue/UnzerRestApi/_support/UnzerRestApiGlueTester.php
@@ -0,0 +1,26 @@
+<?php
+namespace SprykerEcoTest\Glue\UnzerRestApi;
+
+/**
+ * Inherited Methods
+ * @method void wantToTest($text)
+ * @method void wantTo($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method void pause()
+ *
+ * @SuppressWarnings(PHPMD)
+*/
+class UnzerRestApiGlueTester extends \Codeception\Actor
+{
+    use _generated\UnzerRestApiGlueTesterActions;
+
+    /**
+     * Define custom actions here
+     */
+}
