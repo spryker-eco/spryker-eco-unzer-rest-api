baf8be99cd51938fcd73ab45c5f3af52776b6179
Kostiantyn Kichehlov <81977971+kkicheglovspryker@users.noreply.github.com>
2023-06-23 14:51:03 +0300

    CC-29787 Fixed mapper checks if no Unzer credentials set. (#10)
    CC-29787 Fixed mapper checks if no Unzer credentials set.

--- src/SprykerEco/Glue/UnzerRestApi/Plugin/CheckoutRestApi/UnzerCheckoutDataResponseMapperPlugin.php
+++ src/SprykerEco/Glue/UnzerRestApi/Plugin/CheckoutRestApi/UnzerCheckoutDataResponseMapperPlugin.php
@@ -22,6 +22,7 @@ class UnzerCheckoutDataResponseMapperPlugin extends AbstractPlugin implements Ch
      * {@inheritDoc}
      * - Requires `RestCheckoutDataTransfer.quote.unzerCredentials.unzerKeypair.publicKey` to be set while `RestCheckoutDataTransfer.quote.unzerCredentials` is specified.
      * - Maps `RestCheckoutDataTransfer.unzerCredentials.unzerKeypair.publicKey` to `RestCheckoutDataResponseAttributesTransfer.unzerPublicKey` while `RestCheckoutDataTransfer.unzerCredentials` is specified.
+     * - Does nothing if `RestCheckoutDataTransfer.quote.unzerCredentials.unzerKeypair` is not set.
      *
      * @api
      *
